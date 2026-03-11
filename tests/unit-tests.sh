86510cd7e0573b71bc324fd3b12808f0110af8e5
CodeByKyle <kyle@codebykyle.com>
2026-03-11 14:56:26 -0400

    Update workflow and tests

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 
 # Configuration options for this script
-export TESTS_DATABASE=${TESTS_DATABASE:-"testing"}
-export TESTS_ADDONS=${TESTS_ADDONS:-"all"}
-export TESTS_IMAGE_TAG=${TESTS_IMAGE_TAG:-"testing-image"}
-export TESTS_DOCKERFILE=${TESTS_DOCKERFILE:-"Dockerfile"}
-export TESTS_WORKDIR=${TESTS_WORKDIR:-"../src"}
+CALLER_DIR="$(pwd)"
 
-export TESTS_POSTGRES_IMAGE=${TESTS_POSTGRES_SERVER_VERSION:-"postgres:17"}
-export TESTS_ODOO_CONTAINER_NAME=${TESTS_ODOO_CONTAINER_NAME:-"odoo_testing_container"}
-export TESTS_POSTGRES_CONTAINER_NAME=${TESTS_POSTGRES_CONTAINER_NAME:-"${TESTS_ODOO_CONTAINER_NAME}_db"}
+export TESTS_DATABASE="${TESTS_DATABASE:-testing}"
+export TESTS_ADDONS="${TESTS_ADDONS:-all}"
+export TESTS_IMAGE_TAG="${TESTS_IMAGE_TAG:-testing-image}"
+export TESTS_DOCKERFILE="${TESTS_DOCKERFILE:-Dockerfile}"
+export TESTS_WORKDIR="${TESTS_WORKDIR:-${CALLER_DIR}}"
 
-export TESTS_DB_HOST=${TESTS_DB_HOST:-"${TESTS_POSTGRES_CONTAINER_NAME}"}
-export TESTS_DB_USER=${TESTS_DB_USER:-"odoo"}
-export TESTS_DB_PASSWORD=${TESTS_DB_PASSWORD:-"odoo"}
-export TESTS_DB_NAME=${TESTS_DB_NAME:-"testing"}
-export TESTS_TEST_TAGS=${TESTS_TEST_TAGS:-"-/base:TestRealCursor.test_connection_readonly,-/base:test_search.test_13_m2o_order_loop_multi,-/base:TestOrmCache.test_signaling_01_multiple,-/base:TestOrmCache.test_signaling_01_single"}
-export TESTS_SKIP_BUILD=${TESTS_SKIP_BUILD:-"false"}
-export ODOO_VERSION=${ODOO_VERSION:-"19.0"}
+export TESTS_POSTGRES_IMAGE="${TESTS_POSTGRES_SERVER_VERSION:-postgres:17}"
+export TESTS_ODOO_CONTAINER_NAME="${TESTS_ODOO_CONTAINER_NAME:-odoo_testing_container}"
+export TESTS_POSTGRES_CONTAINER_NAME="${TESTS_POSTGRES_CONTAINER_NAME:-${TESTS_ODOO_CONTAINER_NAME}_db}"
 
-# Ensure we do not have a lingering odoo_testing_db
+export TESTS_DB_HOST="${TESTS_DB_HOST:-${TESTS_POSTGRES_CONTAINER_NAME}}"
+export TESTS_DB_USER="${TESTS_DB_USER:-odoo}"
+export TESTS_DB_PASSWORD="${TESTS_DB_PASSWORD:-odoo}"
+export TESTS_DB_NAME="${TESTS_DB_NAME:-testing}"
+export TESTS_TEST_TAGS="${TESTS_TEST_TAGS:--/base:TestRealCursor.test_connection_readonly,-/base:test_search.test_13_m2o_order_loop_multi,-/base:TestOrmCache.test_signaling_01_multiple,-/base:TestOrmCache.test_signaling_01_single}"
+export TESTS_SKIP_BUILD="${TESTS_SKIP_BUILD:-false}"
+export ODOO_VERSION="${ODOO_VERSION:-19.0}"
+
+# Ensure we do not have a lingering postgres container
 docker rm -f "${TESTS_POSTGRES_CONTAINER_NAME}" 2>/dev/null || true
 
 # Optionally skip building the image
 if [ "${TESTS_SKIP_BUILD}" != "true" ]; then
-  # Build the testing image
+  # Build the testing image from the caller directory
   docker build -t "${TESTS_IMAGE_TAG}" \
-    --build-arg ODOO_VERSION=${ODOO_VERSION} \
+    --build-arg ODOO_VERSION="${ODOO_VERSION}" \
     -f "${TESTS_WORKDIR}/${TESTS_DOCKERFILE}" \
     "${TESTS_WORKDIR}"
 
@@ -37,9 +39,8 @@ if [ "${TESTS_SKIP_BUILD}" != "true" ]; then
     exit $BUILD_EXIT_CODE
   fi
 else
-  echo "Skipping image build as SKIP_BUILD is set to true."
+  echo "Skipping image build as TESTS_SKIP_BUILD is set to true."
 fi
-
 
 # Start the database as a daemon
 docker run -d \
@@ -56,14 +57,10 @@ while ! docker inspect -f '{{.State.Running}}' "${TESTS_POSTGRES_CONTAINER_NAME}
   sleep 1
 done
 
-# Get the first argument to the script or use default config
-CONFIG_FILE="${1:-$(pwd)/../src/odoo.conf}"
+# Use first arg if passed, otherwise resolve relative to caller dir
+CONFIG_FILE="${1:-${CALLER_DIR}/config/odoo.conf}"
 
-# Test the docker container
-
-# By default, we should ignore the /volumes/addons folder since at build time
-# we wont have anything there, and the upgrade command will fail because
-# this is not a valid addon directory as an empty folder
+# Run the test container
 docker run \
   --name "${TESTS_ODOO_CONTAINER_NAME}" \
   --link "${TESTS_POSTGRES_CONTAINER_NAME}:${TESTS_POSTGRES_CONTAINER_NAME}" \
@@ -84,17 +81,13 @@ docker run \
 # Capture the exit code of the last command
 TEST_EXIT_CODE=$?
 
-# Cleanup our mess
+# Cleanup
 docker stop "${TESTS_POSTGRES_CONTAINER_NAME}" 2>/dev/null || true
 
-# Print our result and return the test status if
-# the status code does not equal 0
+# Print result and return the test status if failed
 if [ $TEST_EXIT_CODE -ne 0 ]; then
   echo "Tests failed (exit code $TEST_EXIT_CODE)"
   exit $TEST_EXIT_CODE
 fi
 
-# Everything passed!
 echo "All tests passed successfully. ${TEST_EXIT_CODE}"
-
-
